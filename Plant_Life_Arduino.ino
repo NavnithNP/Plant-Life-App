b5420d5dff64f2b9e721bb9018cffe0ad67ad49e
NavnithNP <125210234+NavnithNP@users.noreply.github.com>
2023-02-12 11:30:32 -0600

    Add files via upload

---
+++ Plant_Life_Arduino.ino
@@ -0,0 +1,76 @@
+#include <ArduinoBLE.h>           // Bluetooth Library
+#include <DHT.h>
+#include <DHT_U.h>
+//#include <TimeLib.h>
+
+// Initalizing global variables for sensor data to pass onto BLE
+const  char * UUID_myservice          =  "a0c513b8-46c5-49f4-9eae-0407c5012e7b" ; 
+const  char * UUID_mycharacteristic =  "a0c513b9-46c5-49f4-9eae-0407c5012e7b" ;
+
+// BLE Service Name
+BLEService myService ( UUID_myservice ) ;
+
+// BLE Characteristics
+// Syntax: BLE<DATATYPE>Characteristic <NAME>(<UUID>, <PROPERTIES>, <DATA LENGTH>)
+//BLEFloatCharacteristic myCharacteristic ( UUID_mycharacteristic , BLERead | BLENotify ) ;
+BLEFloatCharacteristic ble_temperature ( UUID_mycharacteristic , BLERead | BLENotify ) ;
+
+#define Type DHT22
+int sensePin=2;
+DHT HT(sensePin,Type);
+float tempF;
+//String t;
+
+
+void setup()
+{
+  // Initalizing Sensor
+    HT.begin();
+  //Serial.begin(9600);
+  //while (!Serial);
+     
+    if (!BLE.begin())
+    {
+        //Serial.println("BLE failed to Initiate");
+        delay(500);
+        while (1);
+    }
+
+    // Setting BLE Name
+    BLE.setLocalName("Nano");
+    
+    // Setting BLE Service Advertisment
+    BLE.setAdvertisedService ( myService ) ;
+        
+    // Adding characteristics to BLE Service Advertisment
+    myService.addCharacteristic(ble_temperature);
+    
+    // Adding the service to the BLE stack
+    BLE.addService ( myService ) ;
+
+    // Start advertising
+    BLE.advertise ( ) ;
+    //Serial.println("Bluetooth device is now active, waiting for connections...");
+}
+
+void loop()
+{
+    // Variable to check if cetral device is connected
+    BLEDevice central = BLE.central ( ) ;
+    central.connected ( ) ;
+
+    float tempF = HT.readTemperature(true);
+    ////t = String(tempF) + " F";
+
+    // Writing sensor values to the characteristic
+    ble_temperature.writeValue(tempF);
+
+    // Displaying the sensor values on the Serial Monitor
+    Serial.println("Reading Sensors");
+    Serial.println(tempF);
+    Serial.println("\n");
+      // Serial.print("Disconnected from central: ");
+   // Serial.println(central.address());
+}
+
+
